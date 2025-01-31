b755479411ebba493baa4472cfba1cf4b64dda57
dstRUpvp <podoroga154@gmail.com>
2025-01-31 09:13:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
+  apiKey: "AIzaSyDYLTqZE8RkZS6L_fHgBPnvTVXGNhzC-Ys",
+  authDomain: "playerstats-9d4d5.firebaseapp.com",
   projectId: "playerstats-9d4d5",
-  // Не додаємо приватний ключ в клієнтський код
+  storageBucket: "playerstats-9d4d5.appspot.com",
+  messagingSenderId: "109713539003405136344",
+  appId: "1:109713539003405136344:web:5f9e8f9f9e8f9f9e8f9f9e"
 };
-
 // Ініціалізація Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
