c32753fa42168cd07d0f9a45b5d4fa44cca807bb
dstRUpvp <podoroga154@gmail.com>
2025-03-11 21:51:48 +0200

    Update stats.js

--- stats.js
+++ stats.js
@@ -125,7 +125,7 @@ function createPlayerCards(players) {
                 <div class="stat"><div class="stat-label">Убийств</div><div class="stat-value">${stats.kills}</div></div>
                 <div class="stat"><div class="stat-label">Смертей</div><div class="stat-value">${stats.deaths}</div></div>
                 <div class="kd-ratio"><div class="stat-label">K/D</div><div class="kd-value">${kd}</div></div>
-                <div class="stat"><div class="stat-label">Время игры</div><div class="kd-value">${playtime} час</div></div>
+                <div class="kd-ratio"><div class="stat-label">Время игры</div><div class="kd-value">${playtime} час</div></div>
             </div>
         `;
         playerCards.appendChild(card);
